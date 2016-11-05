2882099ed0c908afdd8f2e76deba563566163148
windy <xfstudio@qq.com>
2016-11-05 14:18:18 +0800

    init commit

---
+++ pages/index/index.js
@@ -0,0 +1,26 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: '\t\t小邀约\n做推广找合作就用商务邀约',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
